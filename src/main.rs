9ab25b5a3c97fd960cf79b838694a6a23271eeea
n01e0 <noleo@vanillabeans.mobi>
2020-08-16 16:08:19 +0900

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,34 @@
 #[macro_use]
 extern crate clap;
 
-use yara::*;
-use walkdir::WalkDir;
 use std::fs;
+use walkdir::WalkDir;
+use yara::*;
 
 fn main() {
     let matches = clap_app!(watchmen =>
-            (version:       crate_version!())
-            (author:        crate_authors!())
-            (about:         crate_description!())
-            (@arg rule:     +required "yara rule file")
-            (@arg target:   +required "Scan target file")
-        ).get_matches();
+        (version:       crate_version!())
+        (author:        crate_authors!())
+        (about:         crate_description!())
+        (@arg rule:     +required "yara rule file")
+        (@arg target:   +required "Scan target file")
+    )
+    .get_matches();
 
     let mut compiler = Compiler::new().unwrap();
-    compiler.add_rules_file(matches.value_of("rule").unwrap()).expect("Can't add rules");
+    compiler
+        .add_rules_file(matches.value_of("rule").unwrap())
+        .expect("Can't add rules");
     let rules = compiler.compile_rules().expect("Can't compile rules");
-    
+
     let target = matches.value_of("target").unwrap();
-    
+
     if fs::metadata(target).expect("Can't get metadata").is_dir() {
         for entry in WalkDir::new(target).into_iter().filter_map(|e| e.ok()) {
             println!("scanning ... {}", entry.path().display());
             scan(&rules, entry.path().to_str().unwrap().into());
         }
     }
-
 }
 
 fn scan(rules: &Rules, path: String) {
@@ -45,7 +47,7 @@ fn scan(rules: &Rules, path: String) {
                     }
                 }
             }
-        },
+        }
         Err(e) => eprintln!("Error in {} -> {}", path, e),
     }
 }
